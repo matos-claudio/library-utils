f4bb6478fc7e0c860fd22ec1b50c3ecc7b13d349
claudiomatos <cloud.devmo@gmail.com>
2024-04-13 22:46:43 -0300

    Adicionando novo metodo

--- LibraryUtils/src/main/java/br/com/claudio/libraryutils/Example.kt
+++ LibraryUtils/src/main/java/br/com/claudio/libraryutils/Example.kt
@@ -5,5 +5,9 @@ class Example {
         fun getName(): String {
             return "Claudio Matos"
         }
+
+        fun sum(n1: Int, n2: Int): Int {
+            return n1 + n2
+        }
     }
 }
